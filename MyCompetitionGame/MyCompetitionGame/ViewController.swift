f08afcece8eb3bedd559c96e363f506d42ee03f7
Thuy Nguyen <46527706+bigdreamer1610@users.noreply.github.com>
2020-09-03 17:25:32 +0700

    third change, fix thread

--- MyCompetitionGame/MyCompetitionGame/ViewController.swift
+++ MyCompetitionGame/MyCompetitionGame/ViewController.swift
@@ -30,26 +30,26 @@ class ViewController: UIViewController {
     }
     func retrieveDataQuestion(){
        // var myList = [Question]()
-        MyDatabase.ref.child("Question").observeSingleEvent(of: .value) { (snapshot) in
+        MyDatabase.ref.child("Question").observeSingleEvent(of: .value) {[weak self] (snapshot) in
+            guard let `self` = self else {
+                return
+            }
             if let snapshots = snapshot.children.allObjects as? [DataSnapshot] {
                 for snap in snapshots {
                     print(snap.key)
-                    let myKey = snap.key
-                    MyDatabase.ref.child("Question").child(myKey).observeSingleEvent(of: .value) { (mySnap) in
-                        let value = snap.value as? NSDictionary
-                        let questionid = value?["questionid"] as? Int
-                        let categoryid = value?["categoryid"] as? Int
-                        let content = value?["content"] as? String
-                        let trueAns = value?["trueAns"] as? String
-                        let falseAns1 = value?["falseAns1"] as? String
-                        let falseAns2 = value?["falseAns2"] as? String
-                        let falseAns3 = value?["falseAns3"] as? String
+                    if let value = snap.value as? [String : Any] {
+                        let questionid = value["questionid"] as? Int
+                        let categoryid = value["categoryid"] as? Int
+                        let content = value["content"] as? String
+                        let trueAns = value["trueAns"] as? String
+                        let falseAns1 = value["falseAns1"] as? String
+                        let falseAns2 = value["falseAns2"] as? String
+                        let falseAns3 = value["falseAns3"] as? String
                         let question = Question(questionid: questionid!, categoryid: categoryid!, content: content!, trueAns: trueAns!, falseAns1: falseAns1!, falseAns2: falseAns2!, falseAns3: falseAns3!)
                         self.questions.append(question)
-                        print(question)
-                        print("Hello: \(self.questions.count)")
                     }
                 }
+                print(self.questions.count)
             }
         }
         
